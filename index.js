5546fbd53c22f6484263dd972fc0ec08248dca33
Abdulrahman Waleed <a.waleed0011@gmail.com>
2021-06-02 16:21:25 +0200

    ..

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/:url", (req, res) => {
+  // return;
+  const url = new URL(req.params['url']);
+  request = new fetch.Request(url);
+  request.headers.set("Origin", new URL(url).origin);
+  fetch(request)
+    .then(async (response) => {
+      let buffer = await response.buffer();
+      res.set("Access-Control-Allow-Origin", url.origin);
+      res.set("Vary", "Origin");
+      res.set("content-type", response.headers['content-type'])
+      res.send(buffer);
+    });
+
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
